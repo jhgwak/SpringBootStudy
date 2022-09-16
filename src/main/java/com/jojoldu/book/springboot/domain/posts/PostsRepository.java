560c97e2af934ae9d88ea4848805a37e76b2396a
jhkgwak <jhk3763@naver.com>
2022-09-16 22:06:14 +0900

    add CREATE

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -1,5 +1,11 @@
 //DB 에 접근할수 있게 만들어주는 DB Layer 접근자임 JPA 에서는 인터페이스라고 보통 ibatis 나 myBats 등에서는 Dao 라고함
 package com.jojoldu.book.springboot.domain.posts;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
+
 public interface PostsRepository extends JpaRepository<Posts,Long>{
+    @Query("select p from Posts p order by  p.id desc ")
+    List<Posts>findAllDesc();
+
 }
